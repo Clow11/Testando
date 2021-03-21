77cc6109c62b3485d298706d6e709610762532c1
Clow11 <61603409+Clow11@users.noreply.github.com>
2021-03-21 03:18:19 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function home(){
 
     return (
         <div>
-            <h2>oi plenck tudo bom como estas manito? hola?</h2>
+            <h2>oi gente tudo bom como estas manito? hola?</h2>
         </div>
     )
 }
